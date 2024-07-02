f860c42cba92948711dcca35bbcc6bd5abd3adfc
Brijesh Patel <brijeshpatel96@gmail.com>
2024-07-02 11:45:03 +0100

    adding basic files

---
+++ restaurant-finder/app/Provider.js
@@ -0,0 +1,15 @@
+// Used for Google Authentication via NextAuth.js
+
+"use client"
+import React from 'react'
+import { SessionProvider } from "next-auth/react"
+
+function Provider({children}){
+    return(
+        <SessionProvider>
+            {children}
+        </SessionProvider>
+    )
+}
+
+export default Provider
